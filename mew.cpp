463b50e09727e30c8bb02af88f3f76f54824310b
Ruslan Kulagin <rusik.kulagin@yandex.ru>
2017-02-28 23:17:53 +0700

    Last commit

--- mew.cpp
+++ mew.cpp
@@ -5,11 +5,15 @@ int sqr(int n){
 	int y;
 	y=(n*n);
 	return y;}
+int cub(int n){
+	int y;
+	y=(n*n*n);
+	return y;}
 int main () {
 	int x;
 	printf("Vvedite chislo: ");
 	scanf("%d",&x);
-	x=sqr(x);
-	printf("\nKvadrat raven %d",x);
+	x=cub(x);
+	printf("\nCub %d",x);
 system("pause");
 }
